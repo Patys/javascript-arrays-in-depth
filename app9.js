8309d64d19c93814d5460ba20a8c544e6697662e
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 15:04:00 +0200

    mapped trim

--- app9.js
+++ app9.js
@@ -1,7 +1,3 @@
-const items = ["1","2","3","4","5"];
-const mapped = items.map((item) => {
-  return Number(item);
-});
-
-console.log(items);
+const items = [ "   Patryk", "Tomek  "];
+const mapped = items.map(x=>x.trim());
 console.log(mapped);
