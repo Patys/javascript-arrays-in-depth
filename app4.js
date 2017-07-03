2d41f47b97e24a6bf26aabb57f50351a339cac8a
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 14:25:05 +0200

    reduced output by input string with func

--- app4.js
+++ app4.js
@@ -9,3 +9,14 @@ let input = 'name | deslugify | uppercase';
 
 let sections = input.split('|').map(x => x.trim());
 console.log(sections);
+let ref = person[sections[0]];
+let output = sections
+  .slice(1)
+  .reduce((prev, next) => {
+    if(filters[next]) {
+      return filters[next].call(null, prev);
+    }
+    return prev;
+  }, ref);
+
+console.log(output);
