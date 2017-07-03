2963dca9e99217fb926f7619d487780d309138ac
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 15:25:49 +0200

    pasted data

--- app11.js
+++ app11.js
@@ -1,4 +1,10 @@
-const items = [1,2,3,4,5];
-const sum = items.reduce((prev, item) => prev + item, 0);
-
-console.log(sum);
+const users = [
+    {
+        id: '01',
+        name: 'Shane'
+    },
+    {
+        id: '02',
+        name: 'Sally'
+    }
+];
